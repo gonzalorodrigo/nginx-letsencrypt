efc9487fdc9227149d2ae6d2fd2d08f079ea2342
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2018-05-15 17:16:03 -0700

    Added docker build args to the build and push script

--- build_container_push_spin_registry.sh
+++ build_container_push_spin_registry.sh
@@ -3,6 +3,8 @@
 # Builds the container and pushes it to the nersc registry for SPIN.
 
 echo "This command requires to do 'docker login' before... rember to do it"
+echo "Also, if you want NGINX to run with a particular UID and GID run"\
+"./build_container_push_spin_registry.sh (uid_number) (gid_number)"
 
 nersc_user="crd-dst"
 branch=$( git rev-parse --abbrev-ref HEAD )
@@ -12,9 +14,20 @@ if [ -z "$NERSC_USER" ]; then
 else
 	nersc_user=$NERSC_USER
 fi
+build_args=""
+if [ "$#" -ge 1 ]; then
+    build_args="--build-arg SPIN_UID=$1"
+fi
+
+if [ "$#" -ge 2 ]; then
+    build_args="${build_args} --build-arg SPIN_GID=$2"
+fi
+
+echo "Build Args: ${build_args}"
+
 now=`date "+%Y%m%d-%H%M%S"`
 container_name="sciencesearch-nginxv2:$branch-$now"
 spin_registry="registry.spin.nersc.gov"
-docker build --no-cache . -t "$container_name"
+docker build --no-cache . -t "$container_name" ${build_args}
 docker tag ${container_name} ${spin_registry}/${nersc_user}/${container_name}
 docker push ${spin_registry}/${nersc_user}/${container_name}
